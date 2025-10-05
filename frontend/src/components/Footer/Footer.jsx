a907e854190712d5d6d9c2b14cf0c710c4e16139
Shubham <b4mshubham@gmail.com>
2025-10-05 18:30:59 +0530

    package & footer update

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="bg-zinc-800 text-white px-8 py-4">
       <h1 className="text-xl font-semibold text-center">
-        &copy; 2024, Made With Love By BOOKHEAVEN
+        &copy; 2025, Made by Shubham Rakheja
       </h1>
     </div>
   );
